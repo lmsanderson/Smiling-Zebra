ca97898fe78f97c4c04228f752e8eea2e3dd15a8
Matthew Sanderson <matthew@sandersons.id.au>
2012-01-23 20:06:52 +1000

    Version 2.0
    Brand new site

--- code/js/script.js
+++ code/js/script.js
@@ -1,16 +1,10 @@
-/*extend #aboutme*/
-$("#expandcontract").click(function()
-	{
-		if($("#aboutme").is(":hidden"))
-				{
-					$("#aboutme").slideDown("slow");
-				}
-		
-		else
-			{
-				$("#aboutme").slideUp("slow");
-			}
-	}
-);
+/* Author:
+
+*/
+
+
+
+
+
 
 
