7b18d2ce8cb11cdabb18c3151d5945f140efd567
Matthew Sanderson <matthew@sandersons.id.au>
2012-01-29 07:42:16 +1000

    Version 3.0
    Smiling Zebra production!

--- code/js/script.js
+++ code/js/script.js
@@ -1,6 +1,26 @@
-/* Author:
+/*Element Scroller*/
+/*moves an element based on the viewport and centres it*/
+$().ready(function() {
+	var $scrollingDiv = $("#sidebar");
+ 	
+ 	$(window).scroll(function(){			
+		$scrollingDiv
+			.stop()
+			.animate({"marginTop": ($(window).scrollTop() + 30) + "px"}, "slow" );			
+	});
+});
 
-*/
+/*Viewport Anchor Scroller*/
+/*moves the viewport smoothly to a local (in page) anchor*/
+$(".scroll").click(function(event){
+	event.preventDefault();
+    var offset = $($(this).attr('href')).offset().top;
+    $('html, body').animate({scrollTop:offset}, 500);
+});
+
+
+
+
 
 
 
